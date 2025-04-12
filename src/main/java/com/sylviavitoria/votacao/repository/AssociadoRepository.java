f079d412e6b6ed0b5fd86fbd8e3f5b2e7161c196
sylviavitoria <sylviavitoriasilva@gmail.com>
2025-04-12 20:17:29 -0300

    feat(crud-votacao): adicionado método findByCpf na interface AssociadoRepository

--- src/main/java/com/sylviavitoria/votacao/repository/AssociadoRepository.java
+++ src/main/java/com/sylviavitoria/votacao/repository/AssociadoRepository.java
@@ -1,5 +1,7 @@
 package com.sylviavitoria.votacao.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.sylviavitoria.votacao.model.Associado;
 
 @Repository 
 public interface AssociadoRepository extends JpaRepository<Associado, Long> {
-    
+    Optional<Associado> findByCpf(String cpf);
     
 }
